4ec2641be6b886a8f4553872994ea10f56cd3fa9
ZFunnyJoke2 <zmonetov@gmail.com>
2024-05-26 22:58:47 +0300

    git commit --amend

--- src/components/InformationText.vue
+++ src/components/InformationText.vue
@@ -4,27 +4,31 @@
       <div class="content">
         <img src="../assets/img/info-main-sofa.png" alt="Living Room" class="experience-image" />
         <div class="experience-text">
-          <h2>We Provide You The Best Experience</h2>
+          <h2>We Provide You The<br> Best Experience</h2>
           <p>
             You don't have to worry about the result because all of<br>
             these interiors are made by people who are professionals<br>
             in their fields, with an elegant and luxurious style and<br>
             with premium quality materials.
           </p>
-          <a href="#" class="more-info">More Info</a>
+          <button @click="showText = true">More info</button>
+          <div id="Text" v-if="showText">Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br>
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</div>
         </div>
       </div>
     </section>
 
     <section class="materials">
       <div class="content">
-        <h2>Very Serious Materials For Making Furniture</h2>
+        <h2>Very Serious Materials<br> For Making Furniture</h2>
         <p>
           Because panto was very serious about designing furniture<br>
           for our environment, using a very expensive and famous<br>
           capital but at a relatively low price
         </p>
-        <a href="#" class="more-info">More Info</a>
+        <button id="showText">More info</button>
+        <div id="Text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br>
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</div>
         <div class="material-images">
           <img src="../assets/img/info-chair.png" alt="chair"/>
           <img src="../assets/img/info-sofa.png" alt="sofa"/>
@@ -52,8 +56,7 @@ section {
   padding: 2rem 0;
 }
 
-.experience,
-.materials {
+.experience{
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -66,26 +69,22 @@ section {
   align-items: flex-start;
 }
 
-.experience .experience-image {
+.experience-image {
   max-width: 600px;
   margin-right: 2rem;
 }
 
 .experience-text h2,
 .materials h2 {
-  font-size: 24px;
+  font-size: 42px;
   margin-bottom: 1rem;
+  color: #1E1E1E;
 }
 
 .experience-text p,
 .materials p {
-  font-size: 16px;
+  font-size: 18px;
   margin-bottom: 1rem;
-}
-
-.more-info {
-  color: #f9a825;
-  text-decoration: none;
 }
 
 .material-images {
